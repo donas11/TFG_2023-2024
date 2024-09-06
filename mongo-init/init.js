e4bed6aef7feac78e383f530e30d13db43f837e2
Alejandro Manuel do Nascimento Rodríguez <donas11@gmail.com>
2024-09-06 20:31:45 +0200

    archivo init.js de mongo

---
+++ mongo-init/init.js
@@ -0,0 +1,6 @@
+use CIE10
+db.createCollection('SECCION')
+db.createCollection('ENFERMEDAD')
+db.createCollection('ENFERMEDAD_EXCLUYE')
+db.createCollection('ENFERMEDAD_EXCLUYE_ENFERMEDAD')
+db.createCollection('ENFERMEDAD_EXCLUYE_SECCION')
